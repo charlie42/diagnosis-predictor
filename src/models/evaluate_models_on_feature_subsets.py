ed23e6cb2e4df19e7283ecc53113226619448494
charlie42 <oninfrared@gmail.com>
2022-12-15 11:52:23 +0100

    add optional sfs if model is not lr

--- src/models/evaluate_models_on_feature_subsets.py
+++ src/models/evaluate_models_on_feature_subsets.py
@@ -40,9 +40,9 @@ def fit_classifier_on_subset_of_features(best_classifiers, diag, X, y):
     new_classifier.fit(X, y)
     return new_classifier
 
-def make_performance_table(performances_on_subsets, number_of_features_to_check):
+def make_performance_table(performances_on_subsets):
     performances_on_subsets = pd.DataFrame.from_dict(performances_on_subsets)
-    performances_on_subsets.index = range(1, number_of_features_to_check+1)
+    performances_on_subsets.index = range(1, len(performances_on_subsets)+1)
     performances_on_subsets = performances_on_subsets.rename(columns={"index": "Diagnosis"})
     
     return performances_on_subsets
@@ -71,39 +71,39 @@ def make_sens_spec_table(performance_table):
     new_columns = get_diags_in_order_of_auc_on_max_features(performance_table)
     sens_spec_table[new_columns].to_csv(reports_dir+'sens_spec_on_subsets.csv')
 
-def get_top_n_feaures(n, importances, diag):
-    features_up_top_n = importances[diag][:n]
+def get_top_n_features(feature_subsets, diag, n):
+    features_up_top_n = feature_subsets[diag][n]
     return features_up_top_n
 
-def re_train_models_on_feature_subsets_per_output(diag, number_of_features_to_check, importances, datasets, best_classifiers):
+def re_train_models_on_feature_subsets_per_output(diag, feature_subsets, datasets, best_classifiers):
     classifiers_on_feature_subsets = {}
 
-    for nb_features in range(1, number_of_features_to_check+1):
+    for nb_features in feature_subsets[diag].keys():
         X_train, y_train = datasets[diag]["X_train_train"], datasets[diag]["y_train_train"]
 
         # Create new pipeline with the params of the best classifier (need to re-train the imputer on less features)
-        top_n_features = get_top_n_feaures(nb_features, importances, diag)
+        top_n_features = get_top_n_features(feature_subsets, diag, nb_features)
         new_classifier = fit_classifier_on_subset_of_features(best_classifiers, diag, X_train[top_n_features], y_train)
         classifiers_on_feature_subsets[nb_features] = new_classifier
     
     return classifiers_on_feature_subsets
 
-def re_train_models_on_feature_subsets(importances, datasets, best_classifiers, number_of_features_to_check):
+def re_train_models_on_feature_subsets(feature_subsets, datasets, best_classifiers):
     classifiers_on_feature_subsets = {}
-    for diag in importances.keys():
+    for diag in feature_subsets.keys():
         print("Re-training models on feature subsets for output: " + diag)
-        classifiers_on_feature_subsets[diag] = re_train_models_on_feature_subsets_per_output(diag, number_of_features_to_check, importances, datasets, best_classifiers)
+        classifiers_on_feature_subsets[diag] = re_train_models_on_feature_subsets_per_output(diag, feature_subsets, datasets, best_classifiers)
         
     return classifiers_on_feature_subsets
 
-def calculate_thresholds_for_feature_subsets_per_output(diag, importances, classifiers_on_feature_subsets, datasets, number_of_features_to_check):
+def calculate_thresholds_for_feature_subsets_per_output(diag, feature_subsets, classifiers_on_feature_subsets, datasets):
     X_train, y_train = datasets[diag]["X_train_train"], datasets[diag]["y_train_train"]
     X_val, y_val = datasets[diag]["X_val"], datasets[diag]["y_val"]
 
     thresholds_on_feature_subsets = {}
 
-    for nb_features in range(1, number_of_features_to_check+1):
-        top_n_features = get_top_n_feaures(nb_features, importances, diag)
+    for nb_features in feature_subsets[diag].keys():
+        top_n_features = get_top_n_features(feature_subsets, diag, nb_features)
 
         thresholds_on_feature_subsets[nb_features] = models.calculate_threshold(
             classifiers_on_feature_subsets[diag][nb_features], 
@@ -115,13 +115,13 @@ def calculate_thresholds_for_feature_subsets_per_output(diag, importances, class
 
     return thresholds_on_feature_subsets
 
-def calculate_thresholds_for_feature_subsets(importances, classifiers_on_feature_subsets, datasets, number_of_features_to_check):
+def calculate_thresholds_for_feature_subsets(feature_subsets, classifiers_on_feature_subsets, datasets):
     thresholds_on_feature_subsets = {}
-    for diag in importances.keys():
-        thresholds_on_feature_subsets[diag] = calculate_thresholds_for_feature_subsets_per_output(diag, importances, classifiers_on_feature_subsets, datasets, number_of_features_to_check)
+    for diag in feature_subsets.keys():
+        thresholds_on_feature_subsets[diag] = calculate_thresholds_for_feature_subsets_per_output(diag, feature_subsets, classifiers_on_feature_subsets, datasets)
     return thresholds_on_feature_subsets
 
-def get_performances_on_feature_subsets_per_output(diag, importances, classifiers_on_feature_subsets, thresholds_on_feature_subsets, datasets, number_of_features_to_check, use_test_set):
+def get_performances_on_feature_subsets_per_output(diag, feature_subsets, classifiers_on_feature_subsets, thresholds_on_feature_subsets, datasets, use_test_set):
 
     if use_test_set == 1:
         X_test, y_test = datasets[diag]["X_test"], datasets[diag]["y_test"]
@@ -130,9 +130,9 @@ def get_performances_on_feature_subsets_per_output(diag, importances, classifier
 
     metrics_on_subsets = []
     
-    for nb_features in range(1, number_of_features_to_check+1):
+    for nb_features in feature_subsets[diag].keys():
         # Create new pipeline with the params of the best classifier (need to re-train the imputer on less features)
-        top_n_features = get_top_n_feaures(nb_features, importances, diag)
+        top_n_features = get_top_n_features(feature_subsets, diag, nb_features)
         new_classifier = classifiers_on_feature_subsets[diag][nb_features]
         new_threshold = thresholds_on_feature_subsets[diag][nb_features]
         metrics, metric_names = models.get_metrics(new_classifier, new_threshold, X_test[top_n_features], y_test)
@@ -144,33 +144,31 @@ def get_performances_on_feature_subsets_per_output(diag, importances, classifier
 
     return metrics_on_subsets
 
-def get_performances_on_feature_subsets(features_in_importance_order, datasets, best_classifiers, number_of_features_to_check, use_test_set):
-    classifiers_on_feature_subsets = re_train_models_on_feature_subsets(features_in_importance_order, datasets, best_classifiers, number_of_features_to_check)
-    thresholds_on_feature_subsets = calculate_thresholds_for_feature_subsets(features_in_importance_order, classifiers_on_feature_subsets, datasets, number_of_features_to_check)
+def get_performances_on_feature_subsets(feature_subsets, datasets, best_classifiers, use_test_set):
+    classifiers_on_feature_subsets = re_train_models_on_feature_subsets(feature_subsets, datasets, best_classifiers)
+    thresholds_on_feature_subsets = calculate_thresholds_for_feature_subsets(feature_subsets, classifiers_on_feature_subsets, datasets)
 
     performances_on_subsets = {}
     
-    for diag in features_in_importance_order.keys():
-        performances_on_subsets[diag] = get_performances_on_feature_subsets_per_output(diag, features_in_importance_order, classifiers_on_feature_subsets, thresholds_on_feature_subsets, datasets, number_of_features_to_check, use_test_set)
+    for diag in feature_subsets.keys():
+        performances_on_subsets[diag] = get_performances_on_feature_subsets_per_output(diag, feature_subsets, classifiers_on_feature_subsets, thresholds_on_feature_subsets, datasets, use_test_set)
 
     return performances_on_subsets
 
 
-def main(number_of_features_to_check = 100, models_from_file = 1):
-    number_of_features_to_check = int(number_of_features_to_check)
-    models_from_file = int(models_from_file)
+def main(models_from_file = 1):
 
-    features_in_importance_order = load(models_dir+'features-in-importance-order.joblib')
+    feature_subsets = load(models_dir+'feature-subsets.joblib')
     datasets = load(models_dir+'datasets.joblib')
     best_classifiers = load(models_dir+'best-classifiers.joblib')
 
     if models_from_file == 1:
         performances_on_feature_subsets = load(models_dir+'performances-on-feature-subsets.joblib')    
     else:
-        performances_on_feature_subsets = get_performances_on_feature_subsets(features_in_importance_order, datasets, best_classifiers, number_of_features_to_check, use_test_set = 1)
+        performances_on_feature_subsets = get_performances_on_feature_subsets(feature_subsets, datasets, best_classifiers, use_test_set = 1)
         dump(performances_on_feature_subsets, models_dir+'performances-on-feature-subsets.joblib')
 
-    performance_table = make_performance_table(performances_on_feature_subsets, number_of_features_to_check)
+    performance_table = make_performance_table(performances_on_feature_subsets)
     print(performance_table)
     performance_table.to_csv(reports_dir+'performances-on-feature-subsets.csv')
 
@@ -178,4 +176,4 @@ def main(number_of_features_to_check = 100, models_from_file = 1):
     make_sens_spec_table(performance_table)
 
 if __name__ == "__main__":
-    main(sys.argv[1], sys.argv[2])
+    main(sys.argv[1])
