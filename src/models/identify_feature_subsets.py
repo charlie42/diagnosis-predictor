662232eeecbba8e003f0863503294a7dfa2b8bdd
charlie42 <oninfrared@gmail.com>
2022-12-16 15:51:20 +0100

    CODE: feature subsets from file, write to text file

--- src/models/identify_feature_subsets.py
+++ src/models/identify_feature_subsets.py
@@ -5,7 +5,7 @@ os.environ["PYTHONWARNINGS"] = "ignore::UserWarning" # Seems to be the only way 
 from IPython.core import ultratb
 sys.excepthook = ultratb.FormattedTB(color_scheme='Neutral', call_pdb=False)
 
-from joblib import dump
+from joblib import load, dump
 
 # To import from parent directory
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
@@ -33,17 +33,28 @@ def get_feature_subsets(best_classifiers, datasets, number_of_features_to_check)
             feature_subsets[diag] = models.get_feature_subsets_from_sfs(diag, best_classifiers, datasets, number_of_features_to_check)
     return feature_subsets
 
+def create_dir_if_not_exists(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-def main(number_of_features_to_check = 50):
+def write_feature_subsets_to_text_file(feature_subsets):
+    for diag in feature_subsets.keys():
+        create_dir_if_not_exists(reports_dir+diag)
+        with open('feature-subsets.txt', 'w') as f:
+            print(feature_subsets[diag], file=f)
+
+def main(number_of_features_to_check = 50, importances_from_file = 0):
     number_of_features_to_check = int(number_of_features_to_check)
+    importances_from_file = int(importances_from_file)
 
-    from joblib import load
     best_classifiers = load(models_dir+'best-classifiers.joblib')
-
     datasets = load(models_dir+'datasets.joblib')
 
-    feature_subsets = get_feature_subsets(best_classifiers, datasets, number_of_features_to_check)
-    dump(feature_subsets, models_dir+'feature-subsets.joblib')
+    if importances_from_file == 1:
+        feature_subsets = load(models_dir+'feature-subsets.joblib')
+    else:
+        feature_subsets = get_feature_subsets(best_classifiers, datasets, number_of_features_to_check)
+        dump(feature_subsets, models_dir+'feature-subsets.joblib')
 
 if __name__ == "__main__":
     main(sys.argv[1])
