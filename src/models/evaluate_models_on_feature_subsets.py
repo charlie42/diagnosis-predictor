975eec3ca721dfe911935d8c52b90e7665f0dcce
charlie42 <oninfrared@gmail.com>
2022-12-20 15:15:29 +0100

    CODE: refactor -clean models and reports dir in the rest of the project

--- src/models/evaluate_models_on_feature_subsets.py
+++ src/models/evaluate_models_on_feature_subsets.py
@@ -20,12 +20,24 @@ from joblib import dump, load
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
+import models, util
 
-import models
+def set_up_directories(keep_old_re_trained_models=0):
 
-data_processed_dir = "data/processed/"
-models_dir = "models/"
-reports_dir = "reports/"
+    input_data_dir = "data/train_models/"
+    input_models_dir = "models/" + "train_models/"
+    input_reports_dir = "reports/" + "identify_feature_subsets/"
+
+    output_models_dir = "models/" + "evaluate_models_on_feature_subsets/"
+    util.create_dir_if_not_exists(output_models_dir)
+
+    output_reports_dir = "reports/" + "evaluate_models_on_feature_subsets/"
+    util.create_dir_if_not_exists(output_reports_dir)
+
+    if keep_old_re_trained_models == 0:
+        util.clean_dirs([output_models_dir, output_reports_dir]) # Remove old models and reports
+
+    return {"input_data_dir": input_data_dir,  "input_models_dir": input_models_dir, "output_models_dir": output_models_dir, "input_reports_dir": input_reports_dir, "output_reports_dir": output_reports_dir}
 
 def get_best_thresholds(best_classifiers, datasets):
     best_thresholds = models.find_best_thresholds(
@@ -51,7 +63,7 @@ def get_diags_in_order_of_auc_on_max_features(performance_table):
     roc_table =  performance_table.applymap(lambda x: x[0]).iloc[::-1]
     return roc_table.columns[roc_table.loc[roc_table.first_valid_index()].argsort()[::-1]]
 
-def make_auc_table(performance_table):
+def make_auc_table(performance_table, output_reports_dir):
     roc_table = performance_table.applymap(lambda x: x[0])
     print("Before inverse order of rows:")
     print(roc_table)
@@ -61,15 +73,15 @@ def make_auc_table(performance_table):
     print(roc_table)
     # Sort columns by score on 100 features (first row)
     new_columns = get_diags_in_order_of_auc_on_max_features(performance_table)
-    roc_table[new_columns].to_csv(reports_dir+'auc_on_subsets.csv')
+    roc_table[new_columns].to_csv(output_reports_dir+'auc_on_subsets.csv')
 
-def make_sens_spec_table(performance_table):
+def make_sens_spec_table(performance_table, output_reports_dir):
     sens_spec_table = performance_table.applymap(lambda x: str(x[1]) + "," + str(x[2]))
     # Inverse order of rows
     sens_spec_table = sens_spec_table.iloc[::-1]
     # Sort columns by score on 100 features (first row)
     new_columns = get_diags_in_order_of_auc_on_max_features(performance_table)
-    sens_spec_table[new_columns].to_csv(reports_dir+'sens_spec_on_subsets.csv')
+    sens_spec_table[new_columns].to_csv(output_reports_dir+'sens_spec_on_subsets.csv')
 
 def get_top_n_features(feature_subsets, diag, n):
     features_up_top_n = feature_subsets[diag][n]
@@ -157,23 +169,26 @@ def get_performances_on_feature_subsets(feature_subsets, datasets, best_classifi
 
 
 def main(models_from_file = 1):
+    models_from_file = int(models_from_file)
 
-    feature_subsets = load(models_dir+'feature-subsets.joblib')
-    datasets = load(models_dir+'datasets.joblib')
-    best_classifiers = load(models_dir+'best-classifiers.joblib')
+    dirs = set_up_directories(models_from_file)
+
+    feature_subsets = load(dirs["input_reports_dir"]+'feature-subsets.joblib')
+    datasets = load(dirs["input_data_dir"]+'datasets.joblib')
+    best_classifiers = load(dirs["input_models_dir"]+'best-classifiers.joblib')
 
     if models_from_file == 1:
-        performances_on_feature_subsets = load(models_dir+'performances-on-feature-subsets.joblib')    
+        performances_on_feature_subsets = load(dirs["output_reports_dir"]+'performances-on-feature-subsets.joblib')    
     else:
         performances_on_feature_subsets = get_performances_on_feature_subsets(feature_subsets, datasets, best_classifiers, use_test_set = 1)
-        dump(performances_on_feature_subsets, models_dir+'performances-on-feature-subsets.joblib')
+        dump(performances_on_feature_subsets, dirs["output_reports_dir"]+'performances-on-feature-subsets.joblib')
 
     performance_table = make_performance_table(performances_on_feature_subsets)
     print(performance_table)
-    performance_table.to_csv(reports_dir+'performances-on-feature-subsets.csv')
+    performance_table.to_csv(dirs["output_reports_dir"]+'performances-on-feature-subsets.csv')
 
-    make_auc_table(performance_table)
-    make_sens_spec_table(performance_table)
+    make_auc_table(performance_table, dirs["output_reports_dir"])
+    make_sens_spec_table(performance_table, dirs["output_reports_dir"])
 
 if __name__ == "__main__":
     main(sys.argv[1])
