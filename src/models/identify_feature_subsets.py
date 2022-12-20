5f654b11c357cb42f2b97eeedd3b178ef3bc64de
charlie42 <oninfrared@gmail.com>
2022-12-20 15:15:29 +0100

    CODE: refactor -clean models and reports dir in the rest of the project

--- src/models/identify_feature_subsets.py
+++ src/models/identify_feature_subsets.py
@@ -12,12 +12,21 @@ import json
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
-
 import models, util
 
-data_processed_dir = "data/processed/"
-models_dir = "models/"
-reports_dir = "reports/"
+def set_up_directories(keep_old_importances=0):
+
+    input_data_dir = "data/train_models/"
+    models_dir = "models/" + "train_models/"
+    input_reports_dir = "reports/" + "train_models/"
+
+    output_reports_dir = "reports/" + "identify_feature_subsets/"
+    util.create_dir_if_not_exists(output_reports_dir)
+
+    if keep_old_importances == 0:
+        util.clean_dirs([output_reports_dir]) # Remove old models and reports
+
+    return {"input_data_dir": input_data_dir,  "models_dir": models_dir, "input_reports_dir": input_reports_dir, "output_reports_dir": output_reports_dir}
 
 def get_base_model_from_classifier(classifier):
     return list(classifier.named_steps.keys())[-1]
@@ -34,8 +43,8 @@ def get_feature_subsets(ignore_non_lr_diags, best_classifiers, datasets, number_
             feature_subsets[diag] = models.get_feature_subsets_from_sfs(diag, best_classifiers, datasets, number_of_features_to_check)
     return feature_subsets
 
-def write_feature_subsets_to_text_file(feature_subsets):
-    path = reports_dir+"feature-subsets/"
+def write_feature_subsets_to_text_file(feature_subsets, output_reports_dir):
+    path = output_reports_dir+"feature-subsets/"
     util.create_dir_if_not_exists(path)
     for diag in feature_subsets.keys():
         diag_path = path + diag + '/'
@@ -48,16 +57,18 @@ def main(number_of_features_to_check = 50, importances_from_file = 0, ignore_non
     importances_from_file = int(importances_from_file)
     ignore_non_lr_diags = int(ignore_non_lr_diags)
 
-    best_classifiers = load(models_dir+'best-classifiers.joblib')
-    datasets = load(models_dir+'datasets.joblib')
+    dirs = set_up_directories(importances_from_file)
+
+    best_classifiers = load(dirs["models_dir"]+'best-classifiers.joblib')
+    datasets = load(dirs["input_data_dir"]+'datasets.joblib')
 
     if importances_from_file == 1:
-        feature_subsets = load(models_dir+'feature-subsets.joblib')
+        feature_subsets = load(dirs["output_reports_dir"]+'feature-subsets.joblib')
     else:
         feature_subsets = get_feature_subsets(ignore_non_lr_diags, best_classifiers, datasets, number_of_features_to_check)
-        dump(feature_subsets, models_dir+'feature-subsets.joblib')
+        dump(feature_subsets, dirs["output_reports_dir"]+'feature-subsets.joblib')
     
-    write_feature_subsets_to_text_file(feature_subsets)
+    write_feature_subsets_to_text_file(feature_subsets, dirs["output_reports_dir"])
     
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2], sys.argv[3])
