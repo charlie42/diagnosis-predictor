1c301b40fd13b2d904fbc7441d5966fbf5cca55f
charlie42 <oninfrared@gmail.com>
2022-12-20 14:13:21 +0100

    CODE: refactor - add debug flag and clean models and reports dir in train_models

---
+++ src/util.py
@@ -0,0 +1,20 @@
+import os, shutil
+
+def clean_dir(folder):
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print('Failed to delete %s. Reason: %s' % (file_path, e))
+
+def clean_dirs(folders):
+    for folder in folders:
+        clean_dir(folder)
+
+def create_dir_if_not_exists(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
