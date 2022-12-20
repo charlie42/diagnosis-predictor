8d387d0be237208082a375a886ec6e45d9f41a9f
charlie42 <oninfrared@gmail.com>
2022-12-20 16:56:40 +0100

    CODE: find optimal nb of features

--- src/models/evaluate_models_on_feature_subsets.py
+++ src/models/evaluate_models_on_feature_subsets.py
@@ -13,6 +13,7 @@ from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 from sklearn.base import clone
+from sklearn.model_selection import StratifiedKFold, cross_val_score
 
 from joblib import dump, load
 
@@ -52,28 +53,26 @@ def fit_classifier_on_subset_of_features(best_classifiers, diag, X, y):
     new_classifier.fit(X, y)
     return new_classifier
 
-def make_performance_table(performances_on_subsets):
+def make_performance_table(performances_on_subsets, dir):
     performances_on_subsets = pd.DataFrame.from_dict(performances_on_subsets)
     performances_on_subsets.index = range(1, len(performances_on_subsets)+1)
     performances_on_subsets = performances_on_subsets.rename(columns={"index": "Diagnosis"})
-    
+    performances_on_subsets.to_csv(dir+'performances-on-feature-subsets.csv')
     return performances_on_subsets
 
 def get_diags_in_order_of_auc_on_max_features(performance_table):
     roc_table =  performance_table.applymap(lambda x: x[0]).iloc[::-1]
     return roc_table.columns[roc_table.loc[roc_table.first_valid_index()].argsort()[::-1]]
 
-def make_auc_table(performance_table, output_reports_dir):
-    roc_table = performance_table.applymap(lambda x: x[0])
-    print("Before inverse order of rows:")
-    print(roc_table)
-    # Inverse order of rows
-    print("After inverse order of rows:")
-    roc_table = roc_table.iloc[::-1]
-    print(roc_table)
-    # Sort columns by score on 100 features (first row)
-    new_columns = get_diags_in_order_of_auc_on_max_features(performance_table)
-    roc_table[new_columns].to_csv(output_reports_dir+'auc_on_subsets.csv')
+def make_auc_table(auc_on_subsets, output_reports_dir):
+    print(auc_on_subsets)
+    auc_on_subsets = pd.DataFrame.from_dict(auc_on_subsets)
+    print(auc_on_subsets)
+    auc_on_subsets.index = range(1, len(auc_on_subsets)+1)
+    auc_on_subsets = auc_on_subsets.rename(columns={"index": "Diagnosis"})
+    print(auc_on_subsets)
+    auc_on_subsets.to_csv(output_reports_dir+'cv_auc_on_subsets.csv')
+    return auc_on_subsets
 
 def make_sens_spec_table(performance_table, output_reports_dir):
     sens_spec_table = performance_table.applymap(lambda x: str(x[1]) + "," + str(x[2]))
@@ -86,6 +85,20 @@ def make_sens_spec_table(performance_table, output_reports_dir):
 def get_top_n_features(feature_subsets, diag, n):
     features_up_top_n = feature_subsets[diag][n]
     return features_up_top_n
+
+def get_cv_scores_on_feature_subsets(feature_subsets, datasets, best_classifiers):
+    cv_scores_on_feature_subsets = {}
+    
+    for diag in feature_subsets.keys():
+        print("Getting CV scores on feature subsets for " + diag)
+        cv_scores_on_feature_subsets[diag] = []
+        for nb_features in feature_subsets[diag].keys():
+            X_train, y_train = datasets[diag]["X_train"], datasets[diag]["y_train"]
+            top_n_features = get_top_n_features(feature_subsets, diag, nb_features)
+            new_classifier = make_pipeline(SimpleImputer(missing_values=np.nan, strategy='median'), StandardScaler(), clone(best_classifiers[diag][2]))
+            cv_scores = cross_val_score(new_classifier, X_train[top_n_features], y_train, cv = StratifiedKFold(n_splits=10), scoring='roc_auc')
+            cv_scores_on_feature_subsets[diag].append(cv_scores.mean())
+    return cv_scores_on_feature_subsets
 
 def re_train_models_on_feature_subsets_per_output(diag, feature_subsets, datasets, best_classifiers):
     classifiers_on_feature_subsets = {}
@@ -157,6 +170,7 @@ def get_performances_on_feature_subsets_per_output(diag, feature_subsets, classi
     return metrics_on_subsets
 
 def get_performances_on_feature_subsets(feature_subsets, datasets, best_classifiers, use_test_set):
+    cv_scores_on_feature_subsets = get_cv_scores_on_feature_subsets(feature_subsets, datasets, best_classifiers)
     classifiers_on_feature_subsets = re_train_models_on_feature_subsets(feature_subsets, datasets, best_classifiers)
     thresholds_on_feature_subsets = calculate_thresholds_for_feature_subsets(feature_subsets, classifiers_on_feature_subsets, datasets)
 
@@ -165,8 +179,18 @@ def get_performances_on_feature_subsets(feature_subsets, datasets, best_classifi
     for diag in feature_subsets.keys():
         performances_on_subsets[diag] = get_performances_on_feature_subsets_per_output(diag, feature_subsets, classifiers_on_feature_subsets, thresholds_on_feature_subsets, datasets, use_test_set)
 
-    return performances_on_subsets
+    return performances_on_subsets, cv_scores_on_feature_subsets
 
+def get_optimal_nb_features(auc_table, dir):
+    optimal_nb_features = {}
+    for diag in auc_table.columns:
+        max_score = auc_table[diag].max()
+        optimal_score = max_score - 0.01
+        # Get index of the first row with a score >= optimal_score
+        optimal_nb_features[diag] = auc_table[diag][auc_table[diag] >= optimal_score].index[0]
+    print(optimal_nb_features)
+    util.write_dict_to_file(optimal_nb_features, dir, "optimal-nb-features.txt")
+    return optimal_nb_features
 
 def main(models_from_file = 1):
     models_from_file = int(models_from_file)
@@ -179,16 +203,17 @@ def main(models_from_file = 1):
 
     if models_from_file == 1:
         performances_on_feature_subsets = load(dirs["output_reports_dir"]+'performances-on-feature-subsets.joblib')    
+        cv_scores_on_feature_subsets = load(dirs["output_reports_dir"]+'cv-scores-on-feature-subsets.joblib')
     else:
-        performances_on_feature_subsets = get_performances_on_feature_subsets(feature_subsets, datasets, best_classifiers, use_test_set = 1)
+        performances_on_feature_subsets, cv_scores_on_feature_subsets = get_performances_on_feature_subsets(feature_subsets, datasets, best_classifiers, use_test_set = 1)
         dump(performances_on_feature_subsets, dirs["output_reports_dir"]+'performances-on-feature-subsets.joblib')
+        dump(cv_scores_on_feature_subsets, dirs["output_reports_dir"]+'cv-scores-on-feature-subsets.joblib')
 
-    performance_table = make_performance_table(performances_on_feature_subsets)
-    print(performance_table)
-    performance_table.to_csv(dirs["output_reports_dir"]+'performances-on-feature-subsets.csv')
+    performance_table = make_performance_table(performances_on_feature_subsets, dirs["output_reports_dir"])
+    auc_table = make_auc_table(cv_scores_on_feature_subsets, dirs["output_reports_dir"])
+    sens_spec_table = make_sens_spec_table(performance_table, dirs["output_reports_dir"])
 
-    make_auc_table(performance_table, dirs["output_reports_dir"])
-    make_sens_spec_table(performance_table, dirs["output_reports_dir"])
+    get_optimal_nb_features(auc_table, dirs["output_reports_dir"])
 
 if __name__ == "__main__":
     main(sys.argv[1])
