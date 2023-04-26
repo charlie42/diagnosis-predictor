45fd0b4b92ab84cda8f41a7983d4be4591cc76b4
charlie42 <oninfrared@gmail.com>
2023-04-26 10:13:23 +0200

    replace auc w brier, check calibration

--- src/models/helpers/__init__.py
+++ src/models/helpers/__init__.py
@@ -4,3 +4,4 @@ from helpers.idenitfy_thresholds import *
 from helpers.file_helpers import *
 from helpers.lr_coefficients_helpers import *
 from helpers.get_performance_on_feature_subsets import get_performances_on_feature_subsets
+from helpers.build_calibration_curve import build_calibration_curve
