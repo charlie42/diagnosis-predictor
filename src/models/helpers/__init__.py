9f592c8406cc1c8089f18a20af37f2727a9dc541
charlie42 <oninfrared@gmail.com>
2023-05-03 16:49:17 +0200

    [not finished] add sign to feature subsets, print saturation curves

--- src/models/helpers/__init__.py
+++ src/models/helpers/__init__.py
@@ -4,3 +4,4 @@ from helpers.idenitfy_thresholds import *
 from helpers.file_helpers import *
 from helpers.lr_coefficients_helpers import *
 from helpers.get_performance_on_feature_subsets import get_performances_on_feature_subsets
+from helpers.re_train_models_on_subsets import re_train_models_on_feature_subsets, get_top_n_features
--- src/models/identify_feature_subsets.py
+++ src/models/identify_feature_subsets.py
@@ -14,7 +14,7 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 import models, util
 
-DEBUG_MODE = False
+DEBUG_MODE = True
 
 def build_output_dir_name(params_from_previous_script):
     # Part with the datetime
@@ -37,15 +37,23 @@ def set_up_directories():
     # Output dirs
     params_from_previous_script = models.get_params_from_current_data_dir_name(input_data_dir)
     current_output_dir_name = build_output_dir_name(params_from_previous_script)
+
     output_reports_dir = data_dir + "reports/" + "identify_feature_subsets/" + current_output_dir_name + "/"
     util.create_dir_if_not_exists(output_reports_dir)
 
-    return {"input_data_dir": input_data_dir,  "models_dir": models_dir, "input_reports_dir": input_reports_dir, "output_reports_dir": output_reports_dir}
+    output_models_dir = data_dir + "models/" + "identify_feature_subsets/" + current_output_dir_name + "/"
+    util.create_dir_if_not_exists(output_models_dir)
+
+    return {"input_data_dir": input_data_dir,  "models_dir": models_dir, "input_reports_dir": input_reports_dir, 
+            "output_reports_dir": output_reports_dir, "output_models_dir": output_models_dir}
 
 def set_up_load_directories():
     data_dir = "../diagnosis_predictor_data/"
+
     load_reports_dir = models.get_newest_non_empty_dir_in_dir(data_dir+ "reports/identify_feature_subsets/")
-    return {"load_reports_dir": load_reports_dir}
+    load_models_dir = models.get_newest_non_empty_dir_in_dir(data_dir + "models/identify_feature_subsets/")
+
+    return {"load_reports_dir": load_reports_dir, "load_models_dir": load_models_dir}
 
 def get_feature_subsets(best_estimators, datasets, number_of_features_to_check, dirs):
     feature_subsets = {}
@@ -69,30 +77,68 @@ def fix_data_dict(names_df):
     names_df.index = names_df.index.str.replace(r"ICU_(?!(SR))", "ICU_P_")
     return names_df
 
-def append_feature_names_to_feature_subsets(feature_subsets):
-    names_df = fix_data_dict(pd.read_csv("references/item-names.csv", index_col=1, encoding = "ISO-8859-1", names=["questions", "keys"], sep=","))
-    feature_subsets_with_names = {}
-    for diag in feature_subsets.keys():
-        feature_subsets_with_names[diag] = {}
-        for subset in feature_subsets[diag].keys():
-            # Append item name to each item ID 
-            #   Remove name from assessment from feauture name, only keep item name (already contains assessment name)
-            #   Don't append name to basic demographics, self explanatory item IDs
-            feature_subsets_with_names[diag][subset] = [x + f': {names_df.loc[x.split(",")[1]]["questions"]}' 
-                for x in feature_subsets[diag][subset] 
-                if not x.startswith("Basic_Demos")
-                and not x.endswith("WAS_MISSING")
-                and not x.endswith("financialsupport") # Not in data dictionary
-                and not x.endswith("Panic_A01A") # Not in data dictionary
-                and not x.endswith("Panic_A02A")
-                and not x.endswith("Panic_A02A") 
-                and not x.endswith("Panic_A02B")]
-    return feature_subsets_with_names
+def make_sign_dict(feature_list, estimator):
+    # Re-train models on feature subsets (train_train set), get signs of coefficients
+    #   If sign is positive, item is positively correlated with diagnosis
+    #   If sign is negative, item is negatively correlated with diagnosis
 
-def write_feature_subsets_to_text_file(feature_subsets, output_reports_dir):
+    sign_dict = {}
+
+    # If model doesn't have coeffieicents, make values empty
+    if util.get_base_model_name_from_pipeline(estimator) not in ["logisticregression", "svc"]:
+        for item in feature_list:
+            sign_dict[item] = "?"
+        return sign_dict
+
+    if util.get_base_model_name_from_pipeline(estimator) == "logisticregression":
+        coef = estimator.named_steps["logisticregression"].coef_[0]
+    else:
+        coef = estimator.named_steps["svc"].coef_[0]
+    
+    # Get signs of coefficients
+    for i, item in enumerate(feature_list):
+        if coef[i] > 0:
+            sign_dict[item] = "+"
+        else:
+            sign_dict[item] = "-"
+
+    return sign_dict
+
+def make_name_dict(feature_list):
+    # Append item name to each item ID 
+    #   Remove name from assessment from feauture name, only keep item name (already contains assessment name)
+    #   Don't append name to basic demographics, self explanatory item IDs
+    names_df = fix_data_dict(pd.read_csv("references/item-names.csv", index_col=1, encoding = "ISO-8859-1", names=["questions", "keys"], sep=","))
+    name_dict = {}
+    missing_names = ("WAS_MISSING", "financialsupport", "Panic_A01A", "Panic_A02A", "Panic_A01B", "Panic_A02B") 
+    for item in feature_list:
+        if item.startswith("Basic_Demos") or item.endswith(missing_names):
+            name_dict[item] = ""
+        else:
+            name_dict[item] = names_df.loc[item.split(",")[1]]["questions"]
+    return name_dict
+
+def append_names_and_sign_to_feature_subsets(feature_subsets, estimators_on_subsets):
+    feature_subsets_with_names_and_signs = {}
+    for diag in feature_subsets.keys():
+        feature_subsets_with_names_and_signs[diag] = {}
+        for subset in feature_subsets_with_names_and_signs[diag].keys():
+
+            sign_dict = make_sign_dict(feature_subsets[diag][subset], estimators_on_subsets[diag][subset])
+            name_dict = make_name_dict(feature_subsets[diag][subset])
+
+            feature_subsets_with_names_and_signs[diag][subset] = [f'{sign_dict[x]} {x} ({name_dict[x]})' 
+                                                                  for x in feature_subsets[diag][subset]]
+    return feature_subsets_with_names_and_signs
+
+def write_feature_subsets_with_names_and_signs(feature_subsets, estimators_on_subsets, output_reports_dir):
     path = output_reports_dir+"feature-subsets/"
-    feature_subsets_with_names = append_feature_names_to_feature_subsets(feature_subsets)
-    util.write_two_lvl_dict_to_file(feature_subsets_with_names, path)
+
+    feature_subsets_with_names_and_sign = append_names_and_sign_to_feature_subsets(feature_subsets, estimators_on_subsets)
+
+    print(feature_subsets_with_names_and_sign)
+
+    util.write_two_lvl_dict_to_file(feature_subsets_with_names_and_sign, path)
     
 def main(number_of_features_to_check = 126, importances_from_file = 0):
     number_of_features_to_check = int(number_of_features_to_check)
@@ -106,13 +152,18 @@ def main(number_of_features_to_check = 126, importances_from_file = 0):
     if importances_from_file == 1:
         load_dirs = set_up_load_directories()
         feature_subsets = load(load_dirs["load_reports_dir"]+'feature-subsets.joblib')
+        estimators_on_subsets = load(load_dirs["load_models_dir"]+'estimators-on-subsets.joblib')
 
-        # Save reports to newly created directories
         dump(feature_subsets, dirs["output_reports_dir"]+'feature-subsets.joblib')
+        dump(estimators_on_subsets, dirs["output_models_dir"]+'estimators-on-subsets.joblib')
     else:
         feature_subsets = get_feature_subsets(best_estimators, datasets, number_of_features_to_check, dirs)
-        
-    write_feature_subsets_to_text_file(feature_subsets, dirs["output_reports_dir"])
+        estimators_on_subsets = models.re_train_models_on_feature_subsets(feature_subsets, datasets, best_estimators)    
+                
+        dump(feature_subsets, dirs["output_reports_dir"]+'feature-subsets.joblib')
+        dump(estimators_on_subsets, dirs["output_models_dir"]+'estimators-on-subsets.joblib')
+
+    write_feature_subsets_with_names_and_signs(feature_subsets, estimators_on_subsets, dirs["output_reports_dir"])
     
 if __name__ == "__main__":
     main(sys.argv[1], sys.argv[2])
