c187c6f8bd92774b02b4c97a1f838dfe3b3534b9
charlie42 <oninfrared@gmail.com>
2023-07-10 10:02:44 +0200

    fixes and refactor to merge into main

--- src/util.py
+++ src/util.py
@@ -63,7 +63,6 @@ def get_base_model_name_from_estimator(estimator):
     return estimator.__class__.__name__.lower()
 
 def get_estimator_from_pipeline(pipeline):
-    print("DEBUG get_estimator_from_pipeline pipeline", pipeline)
     return pipeline.steps[-1][1]
 
 def get_base_model_name_from_pipeline(pipeline):
